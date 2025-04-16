37cbefa16871b9f289c1bc7472880ae44397b924
akihiro-210 <b.bal.7zlo.akk@gmail.com>
2025-04-16 17:26:08 +0900

    first comit

---
+++ js/script.js
@@ -0,0 +1,78 @@
+
+// ヘッダーメニュースクロール時の色変更
+  $(window).on("scroll", function () {
+    const sliderHeight = $(".mv,.contact-mv").height();
+    if (sliderHeight - 30 < $(this).scrollTop()) {
+      $(".js-header").addClass("headerColorScroll");
+    } else {
+      $(".js-header").removeClass("headerColorScroll");
+    }
+  });
+
+// ページスクロール時にヘッダー高さ分下げる（セクションタイトルとヘッダーの被り防止）
+  $(function () {
+    // ヘッダーの高さ取得
+    const headerHeight = $(".js-header").height();
+    $('a[href^="#"]').click(function () {
+      const speed = 200;
+      let href = $(this).attr("href");
+      let target = $(href == "#" || href == "" ? "html" : href);
+      // ヘッダーの高さ分下げる
+      let position = target.offset().top - headerHeight;
+      $("body,html").animate({ scrollTop: position }, speed, "linear");
+      return false;
+    });
+  });
+
+// ハンバーガーメニュー
+  $(".js-hamburger,.js-drawer .drawer-menu__item a").click(function () {
+    $(".js-hamburger").toggleClass("is-active");
+    $(".js-drawer").fadeToggle();
+  });
+
+
+// スライダー
+  const swiper = new Swiper(".swiper", {
+    loop: true,
+    effect: "fade",
+    speed: 2000,
+    autoplay:{
+      delay: 2000,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+// タブ
+  $(function () {
+    const tabButton = $(".js-tab-button"),
+      tabContent = $(".js-tab-content");
+    tabButton.on("click", function () {
+      let index = tabButton.index(this);
+      tabButton.removeClass("is-active");
+      $(this).addClass("is-active");
+      tabContent.removeClass("is-active");
+      tabContent.eq(index).addClass("is-active");
+    });
+  });
+
+// モーダル
+  $(function () {
+    const open = $(".js-modal-open"),
+      close = $(".js-modal__close"),
+      modal = $(".js-modal");
+    //開くボタンをクリックしたらモーダルを表示する
+    open.on("click", function () {
+      modal.addClass("is-open");
+    });
+    //閉じるボタンをクリックしたらモーダルを閉じる
+    close.add(modal).on("click", function () {
+      modal.removeClass("is-open");
+    });
+  });
