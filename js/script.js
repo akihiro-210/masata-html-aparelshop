5a350a07152c684dabb7bad65d5c1a165aee8f78
akihiro-210 <b.bal.7zlo.akk@gmail.com>
2025-04-17 14:08:14 +0900

    js修正

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 // ヘッダーメニュースクロール時の色変更
   $(window).on("scroll", function () {
-    const sliderHeight = $(".mv,.contact-mv").height();
+    const sliderHeight = $(".fv,.contact-fv").height();
     if (sliderHeight - 30 < $(this).scrollTop()) {
       $(".js-header").addClass("headerColorScroll");
     } else {
